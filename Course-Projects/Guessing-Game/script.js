8b66db09c09825a895e38b7bdf4c499a9173e765
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-01-25 19:31:47 +0530

    Improve input validation and add quit option in Guessing Game

--- Course-Projects/Guessing-Game/script.js
+++ Course-Projects/Guessing-Game/script.js
@@ -1,18 +1,23 @@
-const number = parseInt(prompt("Welcome! Enter your max number:"));
+let number = parseInt(prompt("Welcome! Enter your max number:"));
+while (!number) {
+  number = parseInt(prompt("Please enter a valid number:"));
+}
 
 const target = Math.floor(Math.random() * number) + 1;
-console.log(target);
 
 let guess = parseInt(prompt("Enter your first guess:"));
 let nofOfGuesses = 1;
 while (true) {
-  if (guess === target) {
-    console.log(`It took you ${nofOfGuesses}.`);
+  if (parseInt(guess) === target) {
+    console.log(`It took you ${nofOfGuesses} guesses.`);
     break;
   } else if (guess < target) {
     guess = prompt("Too Low. Guess Again:");
   } else if (guess > target) {
     guess = prompt("Too High. Guess Again:");
+  } else if (guess === "q") {
+    console.log(`You quit the game after ${nofOfGuesses} guesses.`);
+    break;
   }
 
   nofOfGuesses++;
