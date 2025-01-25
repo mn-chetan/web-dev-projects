fa663b1ab28d3ab06774efb2aedac18716d6163b
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-01-25 19:16:48 +0530

    Fix guessing logic to compare user input against the target number

--- Course-Projects/Guessing-Game/script.js
+++ Course-Projects/Guessing-Game/script.js
@@ -1,16 +1,17 @@
 const number = parseInt(prompt("Welcome! Enter your max number:"));
 
 const target = Math.floor(Math.random() * number) + 1;
+console.log(target);
 
 let guess = parseInt(prompt("Enter your first guess:"));
 let nofOfGuesses = 1;
 while (true) {
-  if (guess === number) {
+  if (guess === target) {
     console.log(`It took you ${nofOfGuesses}.`);
     break;
-  } else if (guess < number) {
+  } else if (guess < target) {
     guess = prompt("Too Low. Guess Again:");
-  } else if (guess > number) {
+  } else if (guess > target) {
     guess = prompt("Too High. Guess Again:");
   }
 
