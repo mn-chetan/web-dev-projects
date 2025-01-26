31f74ea6218623742bdfd1e7bfc6fba7c49e42e4
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-01-26 11:14:04 +0530

    Fix input handling and improve output formatting in Todo List app

--- Course-Projects/Todo-List/script.js
+++ Course-Projects/Todo-List/script.js
@@ -5,20 +5,22 @@ while (true) {
 
   if (action === "quit") {
     console.log("OK, YOU QUIT THE APP");
+    break;
   } else if (action === "new") {
     let todoToAdd = prompt("Enter new todo");
     todos.push(todoToAdd);
   } else if (action === "list") {
-    console.log("*".repeat(8));
+    console.log("*".repeat(12));
     for (let i = 0; i < todos.length; i++) {
       console.log(`${i}: ${todos[i]}`);
     }
-    console.log("*".repeat(8));
+    console.log("*".repeat(12));
   } else if (action === "delete") {
-    let indexOfTodoTORemove = prompt("Enter index of todo to delete");
-    while (!indexOfTodoTORemove) {
-      indexOfTodoTORemove = prompt("Enter index of todo to delete");
+    let indexOfTodoTORemove = parseInt(prompt("Enter index of todo to delete"));
+    while (indexOfTodoTORemove === NaN) {
+      indexOfTodoTORemove = parseInt(prompt("Enter index of todo to delete"));
     }
     todos.splice(indexOfTodoTORemove, 1);
+    console.log("Todo Removed");
   }
 }
