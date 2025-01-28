b020b0f9da32ed97f25be4ad241b625cb61fd0ca
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-01-28 15:20:51 +0000

    Add function to get random computer choice in Rock-Paper-Scissors game

---
+++ Odin-Projects/Rock-Paper-Scissors/script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+  const randomChoice = Math.floor(Math.random() * 3);
+
+  if (randomChoice === 0) return "rock";
+  else if (randomChoice === 1) return "paper";
+  else return "scissors";
+}
