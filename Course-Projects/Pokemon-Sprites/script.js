e145fdbd4bed25d58fc79384f43a308c8a45233b
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-01-31 23:08:34 +0530

    Enhance Pokemon sprites display by adding labels for each sprite

--- Course-Projects/Pokemon-Sprites/script.js
+++ Course-Projects/Pokemon-Sprites/script.js
@@ -3,7 +3,13 @@ const baseUrl =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 for (let i = 1; i < 200; i++) {
+  const pokemon = document.createElement("div");
+  const label = document.createElement("span");
+  label.innerText = `#${i}`;
   const newImg = document.createElement("img");
   newImg.src = `${baseUrl}${i}.png`;
-  container.appendChild(newImg);
+
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+  container.appendChild(pokemon);
 }
