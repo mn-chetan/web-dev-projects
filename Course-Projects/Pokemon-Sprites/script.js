dfdf12c81385966ffdf71d9353f913beb00d8cb4
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-01-31 23:18:38 +0530

    Increase Pokemon sprite count to 210 and add styling for sprite display

--- Course-Projects/Pokemon-Sprites/script.js
+++ Course-Projects/Pokemon-Sprites/script.js
@@ -2,8 +2,9 @@ const container = document.querySelector("#container");
 const baseUrl =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-for (let i = 1; i < 200; i++) {
+for (let i = 1; i <= 210; i++) {
   const pokemon = document.createElement("div");
+  pokemon.classList.add("pokemon");
   const label = document.createElement("span");
   label.innerText = `#${i}`;
   const newImg = document.createElement("img");
