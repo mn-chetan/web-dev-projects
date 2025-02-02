2266d19d63c71c6c443fafc9e086c5d22f0cc5bb
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-02 15:01:50 +0000

    Implement score tracking and display for player and computer in game

--- Odin-Projects/Rock-Paper-Scissors/script.js
+++ Odin-Projects/Rock-Paper-Scissors/script.js
@@ -18,8 +18,44 @@ function playRound() {
       const computerChoice = getComputerChoice();
       computerImg.src = `images/${computerChoice}.jpg`;
       computerImg.alt = `Picture of a ${computerChoice}`;
+
+      roundWinner(
+        e.target.innerText.toLowerCase(),
+        computerChoice.toLowerCase()
+      );
+
+      displayScore();
     }
   });
+}
+
+let humanScore = 0,
+  computerScore = 0;
+
+function roundWinner(playerChoice, computerChoice) {
+  const winPair = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
+
+  if (winPair[playerChoice] === computerChoice) {
+    humanScore++;
+  } else if (winPair[computerChoice] === playerChoice) {
+    computerScore++;
+  }
+}
+
+const player = document.querySelector(
+  ".score div:nth-of-type(1) p:nth-of-type(2)"
+);
+const computer = document.querySelector(
+  ".score div:nth-of-type(2) p:nth-of-type(2)"
+);
+
+function displayScore() {
+  player.innerText = humanScore;
+  computer.innerText = computerScore;
 }
 
 playRound();
