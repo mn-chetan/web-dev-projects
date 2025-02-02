e7579e713019d4be29017bfa4d31c1456e9bc7e7
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-02 15:33:17 +0000

    Refactor game logic to implement score tracking and handle game over condition

--- Odin-Projects/Rock-Paper-Scissors/script.js
+++ Odin-Projects/Rock-Paper-Scissors/script.js
@@ -3,59 +3,67 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-const playerChoices = document.querySelector(".player");
-const playerImg = document.querySelector(".middle section:nth-of-type(1) img");
-const computerImg = document.querySelector(
-  ".middle section:nth-of-type(2) img"
-);
+function playGame() {
+  let humanScore = 0,
+    computerScore = 0;
 
-function playRound() {
-  playerChoices.addEventListener("click", (e) => {
-    if (e.target.nodeName === "BUTTON") {
-      playerImg.src = `images/${e.target.innerText}.jpg`;
-      playerImg.alt = `Picture of a ${e.target.innerText}`;
+  const playerChoices = document.querySelector(".player");
+  const playerImg = document.querySelector(
+    ".middle section:nth-of-type(1) img"
+  );
+  const computerImg = document.querySelector(
+    ".middle section:nth-of-type(2) img"
+  );
+  const player = document.querySelector(
+    ".score div:nth-of-type(1) p:nth-of-type(2)"
+  );
+  const computer = document.querySelector(
+    ".score div:nth-of-type(2) p:nth-of-type(2)"
+  );
 
-      const computerChoice = getComputerChoice();
-      computerImg.src = `images/${computerChoice}.jpg`;
-      computerImg.alt = `Picture of a ${computerChoice}`;
+  function playRound(playerChoice) {
+    playerImg.src = `images/${playerChoice}.jpg`;
+    playerImg.alt = `Picture of a ${playerChoice}`;
 
-      roundWinner(
-        e.target.innerText.toLowerCase(),
-        computerChoice.toLowerCase()
-      );
+    const computerChoice = getComputerChoice();
+    computerImg.src = `images/${computerChoice}.jpg`;
+    computerImg.alt = `Picture of a ${computerChoice}`;
 
-      displayScore();
+    roundWinner(playerChoice.toLowerCase(), computerChoice.toLowerCase());
+    displayScore();
+
+    if (humanScore === 5 || computerScore === 5) {
+      alert("Game over! Refresh the screen to play again.");
+      playerChoices.removeEventListener("click", handleClick);
     }
-  });
-}
-
-let humanScore = 0,
-  computerScore = 0;
-
-function roundWinner(playerChoice, computerChoice) {
-  const winPair = {
-    rock: "scissors",
-    paper: "rock",
-    scissors: "paper",
-  };
-
-  if (winPair[playerChoice] === computerChoice) {
-    humanScore++;
-  } else if (winPair[computerChoice] === playerChoice) {
-    computerScore++;
   }
+
+  function displayScore() {
+    player.innerText = humanScore;
+    computer.innerText = computerScore;
+  }
+
+  function roundWinner(playerChoice, computerChoice) {
+    const winPair = {
+      rock: "scissors",
+      paper: "rock",
+      scissors: "paper",
+    };
+
+    if (winPair[playerChoice] === computerChoice) {
+      humanScore++;
+    } else if (winPair[computerChoice] === playerChoice) {
+      computerScore++;
+    }
+  }
+
+  function handleClick(e) {
+    if (e.target.nodeName === "BUTTON") {
+      playRound(e.target.innerText);
+    }
+  }
+
+  playerChoices.addEventListener("click", handleClick);
 }
 
-const player = document.querySelector(
-  ".score div:nth-of-type(1) p:nth-of-type(2)"
-);
-const computer = document.querySelector(
-  ".score div:nth-of-type(2) p:nth-of-type(2)"
-);
-
-function displayScore() {
-  player.innerText = humanScore;
-  computer.innerText = computerScore;
-}
-
-playRound();
+playGame();
