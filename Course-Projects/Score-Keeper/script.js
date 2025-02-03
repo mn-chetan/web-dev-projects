33c45edfb9b17cbaaf1c7cbd84b6dd42293ea503
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-03 18:06:22 +0530

    Add winner/loser color indication and disable buttons on score limit

--- Course-Projects/Score-Keeper/script.js
+++ Course-Projects/Score-Keeper/script.js
@@ -11,23 +11,37 @@ for (let button of buttons) {
     // Make scores 0 if reset button is clicked
     if (e.target.id === "player-1") {
       if (parseInt(score_1.innerText) === intPoints - 1) {
+        // Disable Buttons
         e.target.setAttribute("disabled", "true");
         e.target.nextElementSibling.setAttribute("disabled", "true");
+
+        // Change color to represent winner and loser
+        score_1.style.color = "green";
+        score_2.style.color = "red";
       }
       score_1.innerText = parseInt(score_1.innerText) + 1;
     } else if (e.target.id === "player-2") {
       if (parseInt(score_2.innerText) === intPoints - 1) {
+        // Diable Buttons
         e.target.setAttribute("disabled", "true");
         e.target.previousElementSibling.setAttribute("disabled", "true");
+
+        // Change color to represent winner and loser
+        score_1.style.color = "red";
+        score_2.style.color = "green";
       }
       score_2.innerText = parseInt(score_2.innerText) + 1;
     } else {
       score_1.innerText = 0;
       score_2.innerText = 0;
+
       e.target.previousElementSibling.removeAttribute("disabled");
       e.target.previousElementSibling.previousElementSibling.removeAttribute(
         "disabled"
       );
+
+      score_1.style.color = "";
+      score_2.style.color = "";
     }
   });
 }
