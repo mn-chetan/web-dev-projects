7742448347b050b9757d246c6ffe2fd4e097bcd6
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-04 10:04:35 +0530

    Add operator mapping and operate function to script.js

--- Odin-Projects/Calculator/script.js
+++ Odin-Projects/Calculator/script.js
@@ -3,3 +3,15 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+let number1, operator, number2;
+
+const operatorMapping = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide,
+};
+
+const operate = (number1, operator, number2) =>
+  operatorMapping[operator](number1, number2);
