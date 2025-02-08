cc2b0d6253c656a51ab0f589b877276d2c7361c7
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-08 12:37:24 +0530

    Implement operator functionality and update display handling in calculator

--- Odin-Projects/Calculator/script.js
+++ Odin-Projects/Calculator/script.js
@@ -12,15 +12,39 @@ const operatorMapping = {
 
 const operate = (num1, num2, operator) => operatorMapping[operator](num1, num2);
 
+const operations = {
+  num1: null,
+  num2: null,
+  operator: null,
+};
+
 const display = document.querySelector(".display");
 const span = document.querySelector(".display span");
 
 const digits = document.querySelectorAll(".digit");
 for (let digit of digits) {
   digit.addEventListener("click", (e) => {
-    console.log(span.clientWidth);
-    if (span.clientWidth <= display.clientWidth) {
+    if (span.clientWidth + e.target.clientWidth < display.clientWidth) {
       span.textContent += e.target.textContent;
     }
   });
 }
+
+const operators = document.querySelectorAll(".operator");
+for (let operator of operators) {
+  operator.addEventListener("click", (e) => {
+    operations.num1 = parseFloat(span.textContent);
+    operations.operator = e.target.textContent;
+    span.textContent = "";
+  });
+}
+
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", () => {
+  operations.num2 = parseFloat(span.textContent);
+  span.textContent = operate(
+    operations.num1,
+    operations.num2,
+    operations.operator
+  );
+});
