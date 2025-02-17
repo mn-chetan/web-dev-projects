a84fcb55dda88fe24f1083fa2f6a7d0b2d348f99
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-17 17:21:20 +0530

    Implement gameBoard module with marker placement and win condition logic

---
+++ Odin-Projects/Tic-Tac-Toe/script.js
@@ -0,0 +1,29 @@
+const gameBoard = () => {
+  const board = new Array(9).fill(null);
+
+  return {
+    getBoard: () => [...board],
+    addMarker: (marker, index) => {
+      if (board[index] === null) board[index] = marker;
+    },
+    isWon: () => {
+      const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+
+      return winningCombos.some((combo) => {
+        const [a, b, c] = combo;
+        return (
+          board[a] !== null && board[a] === board[b] && board[a] === board[c]
+        );
+      });
+    },
+  };
+};
