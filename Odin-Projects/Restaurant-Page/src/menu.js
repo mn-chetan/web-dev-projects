817d6e36d539ec787f39aa485586462185ca8844
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-18 16:55:30 +0530

    Refactor breakfast menu display to create a dedicated breakfast section

--- Odin-Projects/Restaurant-Page/src/menu.js
+++ Odin-Projects/Restaurant-Page/src/menu.js
@@ -8,9 +8,11 @@ function displayMenu() {
 
   //   Breakfast
   const breakfast = document.createElement("div");
+  breakfast.id = "breakfast";
 
   const h3 = document.createElement("h3");
   h3.textContent = "Breakfast";
+  breakfast.appendChild(h3);
 
   const breakfastItem1 = document.createElement("div");
   breakfastItem1.classList.add("menu-item");
@@ -23,7 +25,7 @@ function displayMenu() {
   const price1 = document.createElement("p");
   price1.textContent = "$3";
   breakfastItem1.appendChild(price1);
-  menu.appendChild(breakfastItem1);
+  breakfast.appendChild(breakfastItem1);
 
   const breakfastItem2 = document.createElement("div");
   breakfastItem2.classList.add("menu-item");
@@ -36,25 +38,27 @@ function displayMenu() {
   const price2 = document.createElement("p");
   price2.textContent = "$1.5";
   breakfastItem2.appendChild(price2);
-  menu.appendChild(breakfastItem2);
+  breakfast.appendChild(breakfastItem2);
 
   const breakfastItem3 = document.createElement("div");
   breakfastItem3.classList.add("menu-item");
   const img3 = document.createElement("img");
   img3.src = scrambledEggs;
-  breakfastItem2.appendChild(img3);
+  breakfastItem3.appendChild(img3);
   const name3 = document.createElement("p");
   name3.textContent = "Scrambled Eggs";
   breakfastItem3.appendChild(name3);
   const price3 = document.createElement("p");
   price3.textContent = "$1.5";
-  breakfastItem2.appendChild(price3);
-  menu.appendChild(breakfastItem3);
+  breakfastItem3.appendChild(price3);
+  breakfast.appendChild(breakfastItem3);
+
+  menu.appendChild(breakfast);
 
   //   Lunch
 
   //   Dinner
-  
+
   document.body.appendChild(menu);
 }
 
