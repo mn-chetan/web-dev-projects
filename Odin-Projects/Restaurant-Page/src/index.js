76f13f906ff12317b04ad49190fddc9b54ddc3fa
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-18 17:02:28 +0530

    Add event listener for navigation buttons to display menu

--- Odin-Projects/Restaurant-Page/src/index.js
+++ Odin-Projects/Restaurant-Page/src/index.js
@@ -2,5 +2,15 @@ import "./styles.css";
 import { loadContent } from "./about";
 import { displayMenu } from "./menu";
 
-// loadContent();
-displayMenu();
+loadContent();
+
+const nav = document.querySelector("#nav");
+nav.addEventListener("click", (e) => {
+  console.log(e);
+  if (e.target.tagName === "BUTTON") {
+    // Menu
+    if (e.target.textContent === "Menu") {
+      displayMenu();
+    }
+  }
+});
