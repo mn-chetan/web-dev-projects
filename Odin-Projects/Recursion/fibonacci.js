f264b329689e1bd2cbb6cb170fc58a1714c991d5
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-02-20 14:13:31 +0530

    Add recursive Fibonacci sequence implementation in fibonacci.js

--- Odin-Projects/Recursion/fibonacci.js
+++ Odin-Projects/Recursion/fibonacci.js
@@ -14,3 +14,14 @@ function fibs(n) {
 
   return sequence;
 }
+
+// Recursive Solution
+function fibsRec(n) {
+  if (n == 1) return [0];
+  if (n == 2) return [0, 1];
+
+  const sequence = fibsRec(n - 1);
+  sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+
+  return sequence;
+}
